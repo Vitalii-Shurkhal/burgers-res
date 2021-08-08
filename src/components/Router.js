7fea3101a480afb7a803766fd36b9dffc6f98afb
Vitalii-Shurkhal <vshurkhal@gmail.com>
2021-08-08 16:00:17 +0200

    upload

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import App from './App';
 import NotFound from './NotFound';
 
 const Router = ()=> {
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route exact path = '/' component = {Landing} />
                 <Route path = '/restaurant/:restaurantId' component = {App} />
                 <Route component = {NotFound} />
             </Switch>
         
-        </BrowserRouter>
+        </HashRouter>
 
     )
 }
