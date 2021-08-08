2dbeb499d0ce5281a9c93848cb5dfdc846318c0a
Vitalii-Shurkhal <vshurkhal@gmail.com>
2021-08-08 15:50:40 +0200

    Updates

---
+++ precache-manifest.be3adec2e8795286f870c722c2bdee90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60fcebeb01ea5e10e75cfc67322dd77",
+    "url": "/burgers-res/index.html"
+  },
+  {
+    "revision": "218deee720a2ab071be2",
+    "url": "/burgers-res/static/css/main.69f7e651.chunk.css"
+  },
+  {
+    "revision": "ec6142ee12430ee76940",
+    "url": "/burgers-res/static/js/2.19fa4e6c.chunk.js"
+  },
+  {
+    "revision": "218deee720a2ab071be2",
+    "url": "/burgers-res/static/js/main.9c100baa.chunk.js"
+  },
+  {
+    "revision": "e633af04299ced7e330d",
+    "url": "/burgers-res/static/js/runtime-main.0198ba3f.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/burgers-res/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/burgers-res/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/burgers-res/static/media/up.551b5c6b.svg"
+  }
+]);
